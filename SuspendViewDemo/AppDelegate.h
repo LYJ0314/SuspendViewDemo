ae4d7281c3ebe8f23dbbe6de86d3c7015db4fbc3
cchhjj <416387900@qq.com>
2016-12-13 10:10:42 +0800

    Initial Commit

---
+++ SuspendViewDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SuspendViewDemo
+//
+//  Created by cchhjj on 16/12/13.
+//  Copyright © 2016年 CanHe Chen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SuspendViewDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SuspendViewDemo
+//
+//  Created by cchhjj on 16/12/13.
+//  Copyright © 2016年 CanHe Chen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
